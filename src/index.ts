d80094063f80660dafc09ce05664f84adec52c9a
Ethan Spurlock <ethan@spurlock.io>
2022-02-16 16:05:11 -0600

    fix physics declaration

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ const renderer = new THREE.WebGLRenderer( { antialias: true } );
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
 
-let physics: RapierPhysics;
-
 window.addEventListener( 'resize', onWindowResize, false );
 document.body.appendChild( VRButton.createButton( renderer ) );
 
@@ -18,41 +16,41 @@ init();
 
 async function init() {
 
-    physics = await RapierPhysics.autoImport();
-    physics.buildDefaultWorld();
+  const physics = await RapierPhysics.autoImport();
+  physics.buildDefaultWorld();
 
-    // Create a dynamic rigid-body.
-    let rigidBodyDesc = RigidBodyDesc.newDynamic().setTranslation(0.0, 1.0, 0.0);
-    let rigidBody = physics.world.createRigidBody(rigidBodyDesc);
+  // Create a dynamic rigid-body.
+  let rigidBodyDesc = RigidBodyDesc.newDynamic().setTranslation(0.0, 1.0, 0.0);
+  let rigidBody = physics.world.createRigidBody(rigidBodyDesc);
 
-    // Create a cuboid collider attached to the dynamic rigidBody.
-    let colliderDesc = ColliderDesc.cuboid(0.5, 0.5, 0.5);
-    let collider = physics.world.createCollider(colliderDesc, rigidBody.handle);
+  // Create a cuboid collider attached to the dynamic rigidBody.
+  let colliderDesc = ColliderDesc.cuboid(0.5, 0.5, 0.5);
+  let collider = physics.world.createCollider(colliderDesc, rigidBody.handle);
+
+  camera.position.z = 1;
+
+  const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+  const material = new THREE.MeshNormalMaterial();
+
+  const mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  renderer.setAnimationLoop( function () {
+
+      // Step the simulation forward.  
+      physics.world.step();
+
+      // Get and print the rigid-body's position.
+      let position = rigidBody.translation();
+      mesh.position.set(position.x, position.y, position.z);
+      mesh.rotation.set(rigidBody.rotation().x, rigidBody.rotation().y, rigidBody.rotation().z);
   
-    camera.position.z = 1;
-
-    const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-    const material = new THREE.MeshNormalMaterial();
-
-    const mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-
-    renderer.setAnimationLoop( function () {
-
-        // Step the simulation forward.  
-        physics.world.step();
+      renderer.render( scene, camera );
   
-        // Get and print the rigid-body's position.
-        let position = rigidBody.translation();
-        mesh.position.set(position.x, position.y, position.z);
-        mesh.rotation.set(rigidBody.rotation().x, rigidBody.rotation().y, rigidBody.rotation().z);
-    
-        renderer.render( scene, camera );
-    
-    } );
+  } );
 
 }
 
