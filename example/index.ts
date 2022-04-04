b4b7a607e04cd8b076b03f9c5307b6314aac3340
Ethan Spurlock <ethan@spurlock.io>
2022-04-03 23:07:59 -0500

    GLTF Importer POC

--- example/index.ts
+++ example/index.ts
@@ -10,12 +10,6 @@ import { sphere } from "../src/shapes/sphere";
 import { cube } from "../src/shapes/cube";
 
 
-const ball = sphere(0.05, 0x0000ff,16, RigidBodyType.Dynamic, Transform.fromPositionRotationScale(new float3([0, 5, 0]), Quaternion.identity, float3.one));
-
-ball.transform.setTranslation(new float3([0,5,0]));
-
-
-
 function sandbox(width : number, height : number): Entity[]
 {
   return[
@@ -34,20 +28,42 @@ function sandbox(width : number, height : number): Entity[]
     sphere(0.05, 0x00ff00, 16, RigidBodyType.Dynamic, Transform.fromPositionRotationScale(new float3([.8, 0.1 + height, 0]), Quaternion.identity, float3.one)),
     sphere(0.05, 0x00ff00, 16, RigidBodyType.Dynamic, Transform.fromPositionRotationScale(new float3([.9, 0.1 + height, 0]), Quaternion.identity, float3.one)),
     sphere(0.05, 0x00ff00, 16, RigidBodyType.Dynamic, Transform.fromPositionRotationScale(new float3([.6, 0.1 + height, .2]), Quaternion.identity, float3.one)),
+    
   ]
 
 }
 
-//TODO: Make a better Enity Array Class with convenient methods
-var scene_array : Entity[] = [
-  ball,
-  ...sandbox(2, 0.5),
-  ...LeftHandEntity,
-  ...RightHandEntity
-];
+async function main()
+{
+  const ball = sphere(0.05, 0x0000ff,16, RigidBodyType.Dynamic, Transform.fromPositionRotationScale(new float3([0, 5, 0]), Quaternion.identity, float3.one));
+
+  ball.transform.setTranslation(new float3([0,5,0]));
+
+  const helmet = await Entity.from_glb_url("DamagedHelmet.glb", Transform.fromPositionRotationScale(new float3([0, 5, 0]), Quaternion.fromEulerXYZ(-90,0,0), new float3([0.1,0.1,0.1])));
+
+
+  //TODO: Make a better Enity Array Class with convenient methods
+  var scene_array : Entity[] = [
+    ball,
+    ...sandbox(2, 0.5),
+    ...LeftHandEntity,
+    ...RightHandEntity,
+    helmet,
+  ];
+
+  const scene = new Scene(scene_array);
+}
+
+main();
 
 
 
-
-const scene = new Scene(scene_array);
-
+//TODO:
+// - Find a scene from turbosquid
+// - split it into different GLTF files for each model
+// - decide on a strategy for VR UI
+// - Build a GLTF selection UI in VR
+// - Make a tool for translation, rotation, scaling
+// - Make a tool for collider creation
+// - Make a tool for rigid body creation
+// - Create the option to export an item to conbiniverse CDN via your conbiniverse account
